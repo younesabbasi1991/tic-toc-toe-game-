939bbe674f94c97260724eedc4138b8015f4a3eb
Younes Abbasi <younesabbasi6@gmail.com>
2025-07-09 09:54:24 +0330

    finishing the name part

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-export default function Player({name , symbol}) {
-const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEdithClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function hanleChange(event) {
+    setPlayerName(event.target.value);
+    console.log(event.target.value);
+  }
+  let edithablePlayerName = <span className="player-name">{playerName}</span>;
 
+  if (isEditing) {
+    edithablePlayerName = (
+      <input type="text" required value={playerName} onChange={hanleChange} />
+    );
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {edithablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edith</button>
+      <button onClick={handleEdithClick}>
+        {!isEditing ? "Edith" : "Save"}
+      </button>
     </li>
   );
 }
